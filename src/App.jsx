05efd4eb9efe9ea0034478de462d9e7bc248ca0c
Artem-Potapov <potapov23500@gmail.com>
2023-06-25 15:43:53 +0300

    add advantage section

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
 import Hero from "./components/Hero";
 import Navbar from "./components/Navbar";
+import Advantages from "./components/Advantages";
+import stability from "./images/Stability.png";
+import fastness from "./images/Fastness.png";
+import helpfulness from "./images/Helpfulness.png";
+import AdvantageCard from "./components/AdvantageCard";
 
 function App() {
 
@@ -10,14 +15,25 @@ function App() {
     { name: "корзина", link: "#" },
   ];
 
-  const navLinksArray = navLinks.map((link) => (
-    <a href={link.link}>{link.name}</a>
+  const advantageData = [
+    {name: "Надёжность", desc:"Мы гарантируем надежность наших услуг.", img:stability},
+    {name: "Поддержка", desc:"Мы гарантируем отличную техподдержку.", img:helpfulness},
+    {name: "Скорость", desc:"Мы гарантируем скорость доставки.", img:fastness},
+  ];
+
+  const navLinksArray = navLinks.map((link, id) => (
+    <a key={id} href={link.link}>{link.name}</a>
   ));
+
+  const advantageCards = advantageData.map((card) => (
+    <AdvantageCard name={card.name} desc={card.desc} img={card.img} />
+  ))
 
   return (
     <>
       <Navbar navLinksArray={navLinksArray} />
       <Hero />
+      <Advantages cards={advantageCards}/>
     </>
   );
 }
